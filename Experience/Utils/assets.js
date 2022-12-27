80ae5bc9074e9d8bb23a2c6fda8a14e958763534
erwinsot <esoto@unal.edu.co>
2022-12-27 13:04:29 -0500

    version 1

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Version1.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4",
+    },
+    {
+        name: "cuadro1",
+        type: "videoTexture",
+        path: "/textures/mid2.mp4",
+    },
+    {
+        name: "cuadro2",
+        type: "videoTexture",
+        path: "/textures/Mariposa.mp4",
+    },
+    {
+        name: "cuadro3",
+        type: "videoTexture",
+        path: "/textures/Cubos.mp4",
+    },
+    
+];
